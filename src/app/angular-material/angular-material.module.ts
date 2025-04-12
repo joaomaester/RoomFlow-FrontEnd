a2382f1d47c14f2a6c16b29ae2dd50b913dadab7
diwelitonmaester <diwelitonmaester@gmail.com>
2025-04-11 22:41:00 -0300

    tela inicial

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import {MatInput} from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   imports: [
     MatTableModule,
     MatInput,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatButtonModule,
   ],
   exports: [
     MatTableModule,
--- src/app/components/pages/gerenciamento/lista-de-usuarios/lista-de-usuarios.component.ts
+++ src/app/components/pages/gerenciamento/lista-de-usuarios/lista-de-usuarios.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { IUsuario } from '../../../../Interfaces/Usuario.interface';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-lista-de-usuarios',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTableModule],
   templateUrl: './lista-de-usuarios.component.html',
   styleUrl: './lista-de-usuarios.component.css'
 })
